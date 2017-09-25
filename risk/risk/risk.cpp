e25f9f3d92e8cf6499f534e524d5390ab5b1acc2
mihaiqc <mihai_qc@hotmail.fr>
2017-09-25 12:16:35 -0400

    Test compiler + Github integration

---
+++ risk/risk/risk.cpp
@@ -0,0 +1,13 @@
+// risk.cpp : Defines the entry point for the console application.
+//
+#include <iostream>
+#include "stdafx.h"
+using namespace std;
+
+
+int main()
+{
+    cout <<"Testing IDE"+" Testing gcc/git IDE integration";
+    return 0;
+}
+
