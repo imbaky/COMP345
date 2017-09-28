c3fab25e9a03ceaae4faae4ece82aa3b11c8615c
Kenny Lee Sin Cheong <kenny.lee28+git@gmail.com>
2017-09-28 18:57:37 -0400

    Implement map classes

--- map/map.h
+++ map/map.h
@@ -12,30 +12,44 @@ public:
 	string name;
 	int posX;
 	int posY;
-	vector<Country *> neighbors;
 
 	Country(string name, int posX, int posY);
+	
+	vector<Country *> getNeighbors();
 	void addNeighbor(Country *country);
 	bool hasNeighbor(Country *country);
 	bool hasNeighbor(string country);
+private:
+	vector<Country *> neighbors;
 };
 
 class Continent {
 public:
 	string name;
-	vector<Country *> countries;
 
 	Continent(string name);
+
+	vector<Country *> getCountries();
+	vector<Continent *> getNeighbors();
 	void addCountry(Country *country);
 	bool hasCountry(Country *country);
 	bool hasCountry(string country);
+	void addNeighbor(Continent *continent);
+	bool hasNeighbor(Continent *continent);
+	bool hasNeighbor(string continent);
+private:
+	vector<Country *> countries;
+	vector<Continent *> neighbors;
 };
 
 class Map {
 public:
-	vector<Continent *> continents;
-
+	vector<Continent *> getContinents();
 	void addContinent(Continent *continent);
+	bool hasContinent(Continent *continent);
+	bool hasContinent(string continent);
+private:
+	vector<Continent *> continents;
 };
 
 #endif
