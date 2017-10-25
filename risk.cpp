a2b2ad3f2e02d715755e008b546b1c3e42cbb32d
pǝɯɐɥoɯ <mo7_abd@hotmail.com>
2017-10-25 12:18:23 -0400

    Created gitignore + add startgame

--- risk.cpp
+++ risk.cpp
@@ -2,15 +2,34 @@
 
 using namespace std;
 
+// function declaration
+void start_game();
+
 int main()
 {
-cout<<"Lets play !\n"<<endl;
 
-cout << "██████╗ ██╗███████╗██╗  ██╗\n"<<
-        "██╔══██╗██║██╔════╝██║ ██╔╝\n"<<
-        "██████╔╝██║███████╗█████╔╝ \n"<<
-        "██╔══██╗██║╚════██║██╔═██╗ \n"<<
-        "██║  ██║██║███████║██║  ██╗\n"<<
-        "╚═╝  ╚═╝╚═╝╚══════╝╚═╝  ╚═╝" << endl;
+    cout<<"Lets play !\n"<<endl;
 
+    cout << "██████╗ ██╗███████╗██╗  ██╗\n"<<
+            "██╔══██╗██║██╔════╝██║ ██╔╝\n"<<
+            "██████╔╝██║███████╗█████╔╝ \n"<<
+            "██╔══██╗██║╚════██║██╔═██╗ \n"<<
+            "██║  ██║██║███████║██║  ██╗\n"<<
+            "╚═╝  ╚═╝╚═╝╚══════╝╚═╝  ╚═╝\n\n" << endl;
+    start_game();
+
+
+}
+
+void start_game(){
+    bool valid_input=false;
+    char input;
+    while(!valid_input){
+        cout<<"Would you like to start the game? (y/n)"<<endl;
+        cin>>input;
+        if(input=='y'){
+            valid_input=true;
+        }else if(input!='n')
+                cerr<<"invalid input !!"<<endl;
+    }
 }
